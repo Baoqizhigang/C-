1fc18f0447d81af865d8a5146692f4f19684d837
QuantumUnicorn <qliu2012@hotmail.com>
2024-06-19 08:24:09 -0700

    Update 3_18_HZOJ107.c

--- 3_18_HZOJ107.c
+++ 3_18_HZOJ107.c
@@ -1,21 +1,26 @@
 /*************************************************************************
-	> while 循环:  
-	while 循环用于在条件为真时重复执行代码块。
-	while (n--) if (1) { ... }
-	while (condition) statement; 形式。
-	if (condition) statement; 形式，1 为真值，表示条件总是成立。
+	> 题目描述:
+	判断一个整数a能否被另一个整数b整除
 
-	> for 循环 :
-	for 循环用于在已知次数的情况下重复执行代码块。
-	for (initialization; condition; increment) statement; 形式。
+	> 输入 :
+	输入两个整数a,b (1 <= a,b <=500)
 
-	> 循环控制 :
-	while 和 for 循环在每次迭代前检查条件表达式，如果条件为真，则继续执行循环体。
-	增量表达式在每次循环结束后执行，更新循环控制变量。
+	> 输出 :
+	a能被b整除输出YES, 否则输出NO
 
-	> 循环嵌套 :
-	嵌套循环用于在一个循环体内嵌套另一个循环体，以处理多维数据结构或复杂的循环逻辑。
+	> 三元运算符 :
+	三元运算符 condition ? expression1 : expression2 是一种简洁的条件表达方式，可以替代简单的 if-else 结构。
+	condition 是一个逻辑表达式，如果 condition 为真（非零），则计算并返回 expression1；否则，计算并返回 expression2。
+
  ************************************************************************/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+
+int main() {
+	int a, b;
+	scanf("%d%d", &a, &b);
+	// 使用三元运算符检查 a 是否可以被 b 整除，并根据结果输出相应的字符串。
+	printf("%s\n", a % b == 0 ? "YES" : "NO");
+	return 0; // 返回 0，表示程序正常结束。
+}
