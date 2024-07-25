91eabd760f994a693eead9465c4bb210ab5915f7
QuantumUnicorn <qliu2012@hotmail.com>
2024-07-25 16:27:32 -0700

    Update 5_5_array.c

--- 5_5_array.c
+++ 5_5_array.c
@@ -1,7 +1,10 @@
-
 /*************************************************************************
 多种数组的初始化和操作方式，以及如何使用 sizeof 运算符计算数组的大小，和如何打印数组元素及其地址。
 每个函数都展示了不同的数组操作和初始化方法。
+
+size_t：是一种无符号整数类型，通常用于表示对象的大小或数组的索引。它的定义在 <stddef.h> 或 <stdio.h> 头文件中。
+特性：size_t 的大小和 unsigned int 或 unsigned long 相同，具体取决于系统的实现。在 32 位系统上，它通常是 32 位无符号整数；在 64 位系统上，它通常是 64 位无符号整数。
+用途：size_t 通常用于返回 sizeof 运算符的结果，表示内存大小和数组索引。
  ************************************************************************/
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -52,10 +55,10 @@ void test4() {
 
 void test5() {
     int a[] = {1, 2, 3, 4, 5, 6, 7}; // 声明并初始化一个数组 a，包含 7 个整数元素
-    // 计算数组 a 的大小
+    // 计算数组 a 的大小 - size_t 通常用于返回 sizeof 运算符的结果，表示内存大小和数组索引
     size_t size = sizeof(a) / sizeof(int); // sizeof(a)：数组 a 所占内存的字节数。sizeof(int)：一个 int 类型的字节数。size：数组元素个数。
-    printf("sizeof(a) / sizeof(int) = %lu\n", size);
-    printf("a = %p\n", a);
+    printf("sizeof(a) / sizeof(int) = %lu\n", size); //使用 %lu 格式说明符输出无符号长整数（size_t）。
+    printf("a = %p\n", a); //打印数组 a 的首地址，使用 %p 格式说明符输出指针地址
     printf("&a[0] = %p\n", &a[0]);
     for (int i = 0; i < size; i++) {
         printf("&a[%d] = %p\n", i, &a[i]);
